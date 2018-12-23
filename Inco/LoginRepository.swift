00ed57fe4be4dc425304d2d741ba7f0ab8e3a98e
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-23 17:07:03 +0900

    login

--- Inco/LoginRepository.swift
+++ Inco/LoginRepository.swift
@@ -4,6 +4,7 @@
 //
 
 import Alamofire
+import Foundation
 
 class LoginRepository {
     static let shared = LoginRepository()
@@ -11,9 +12,14 @@ class LoginRepository {
     private init() {
     }
 
-    func login(user: String, pass: String) {
+    func login(user: String, pass: String, onCompleted: @escaping (Login?) -> Void) {
         APIClient.shared.login(user: user, pass: pass).response {
-            // TODO
+            if let data = $0.data {
+                let login = try! JSONDecoder().decode(Login.self, from: data)
+                onCompleted(login)
+                return
+            }
+            onCompleted(nil)
         }
     }
 }
