0b20c96eb83d8d8701469fb118bf447693359b4a
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-23 12:20:46 +0900

    bind

--- Inco/ViewController.swift
+++ Inco/ViewController.swift
@@ -11,19 +11,21 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var usernameTextField: NSTextField!
+    @IBOutlet weak var passwordTextField: NSSecureTextField!
+
     override func viewDidLoad() {
-    super.viewDidLoad()
+        super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
     }
-
 
     override var representedObject: Any? {
-    didSet {
-    // Update the view, if already loaded.
+        didSet {
+            // Update the view, if already loaded.
+        }
     }
+
+    @IBAction func onClick(_ sender: NSButton) {
     }
-
-
-
 }
