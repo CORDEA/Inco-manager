a91e4b69c6587d2e32f66a6c10c1d21531f04841
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-23 11:27:37 +0900

    initial commit :)

---
+++ Inco/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Inco
+//
+//  Created by Yoshihiro Tanaka on 2018-12-23.
+//  Copyright © 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import Cocoa
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+    }
+
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+    }
+
+
+
+}
---
+++ Inco/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Inco
+//
+//  Created by Yoshihiro Tanaka on 2018-12-23.
+//  Copyright © 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+    }
+
+
+    override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+    }
+
+
+
+}
