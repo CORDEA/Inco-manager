afd4f8ca1d70ef39f98c8eb1edc3715d4697e5d2
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-23 16:49:34 +0900

    define response class

---
+++ Inco/Response/Login.swift
@@ -0,0 +1,10 @@
+//
+// Created by Yoshihiro Tanaka on 2018-12-23.
+// Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import Foundation
+
+struct Login: Decodable {
+    var token: String
+}
