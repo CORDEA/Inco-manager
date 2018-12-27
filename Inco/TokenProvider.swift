a8dabec0877a6d7f1a91e677cd4a8ebebfe60c70
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-27 16:17:09 +0900

    add provider

---
+++ Inco/TokenProvider.swift
@@ -0,0 +1,18 @@
+//
+// Created by Yoshihiro Tanaka on 2018-12-27.
+// Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import Foundation
+
+class TokenProvider {
+    private static let tokenDefaultsKey = "token"
+
+    static func get() -> String {
+        return UserDefaults.standard.string(forKey: tokenDefaultsKey)!
+    }
+
+    static func refresh(token: String) {
+        UserDefaults.standard.set(token, forKey: tokenDefaultsKey)
+    }
+}
