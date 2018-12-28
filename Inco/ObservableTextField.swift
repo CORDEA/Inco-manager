3991f6ad9f0e3469ec395e892d95ecb5fe3c3965
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-28 11:27:58 +0900

    resolve build error

--- Inco/ObservableTextField.swift
+++ Inco/ObservableTextField.swift
@@ -6,16 +6,18 @@
 import Cocoa
 import RxSwift
 
-class ObservableTextField: NSTextFieldDelegate {
+class ObservableTextField: NSObject, NSTextFieldDelegate {
     private let _onTextChanged: PublishSubject<String>
     let onTextChanged: Observable<String>
 
-    init() {
+    override init() {
         _onTextChanged = PublishSubject()
         onTextChanged = _onTextChanged
+        super.init()
     }
 
     func textField(_ textField: NSTextField, textView: NSTextView, shouldSelectCandidateAt index: Int) -> Bool {
         _onTextChanged.onNext(textField.stringValue)
+        return true
     }
 }
