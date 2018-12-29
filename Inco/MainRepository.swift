9f99d7d07e5c02f95df9124fadaf3ef629301d53
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-29 16:21:09 +0900

    add repository

---
+++ Inco/MainRepository.swift
@@ -0,0 +1,13 @@
+//
+// Created by Yoshihiro Tanaka on 2018-12-29.
+// Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import Foundation
+
+class MainRepository {
+    static let shared = MainRepository()
+
+    private init() {
+    }
+}
