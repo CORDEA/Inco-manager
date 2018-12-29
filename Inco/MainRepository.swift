9264f334ab253c6dfa5f1e631ab47e938119b11d
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-29 16:27:01 +0900

    get and decode histories

--- Inco/MainRepository.swift
+++ Inco/MainRepository.swift
@@ -3,11 +3,23 @@
 // Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
 //
 
+import Alamofire
 import Foundation
 
 class MainRepository {
     static let shared = MainRepository()
 
     private init() {
+    }
+
+    func getHistories(onCompleted: @escaping ([History]) -> Void) {
+        APIClient.shared.getHistories().response {
+            if let data = $0.data {
+                let histories = try! JSONDecoder().decode([History].self, from: data)
+                onCompleted(histories)
+                return
+            }
+            onCompleted([])
+        }
     }
 }
